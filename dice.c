b1624a6eafa55ec95ad1b9596c4d8e0f0477eacf
hdebhueg <87011554+hdebhueg@users.noreply.github.com>
2021-07-06 18:58:56 +0900

    Create dice.c

---
+++ dice.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+
+int Dice(int);
+int main(){
+  srand((unsigned int)time(NULL));
+  int v=0;
+  int i;
+  char name[20];
+
+    
+  printf("What is your name?\n");
+  printf(">");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+    
+
+ 
+  printf("Rolling the dice...\n");
+
+  for(i=0;i<2;i++)
+    v=v+Dice(i+1);
+
+  
+  printf("Total value:%d\n",v);
+  
+  if(v>=7) printf("%s won!\n",name);
+  else printf("%s lost!\n",name);
+
+  printf("END\n");
+  
+  return 0;
+}
+
+int Dice(int i){
+  int num;
+  num=rand()%6+1;
+  printf("Die %d:%d\n",i,num);
+  return num;
+}
